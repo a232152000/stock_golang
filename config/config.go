e78524f36cb877d1373d6e1e3043a1526b5e4e9c
a232152000 <a232152000@gmail.com>
2021-04-04 13:28:10 +0800

    config.go拿掉註解

--- config/config.go
+++ config/config.go
@@ -5,16 +5,6 @@ import (
 	"log"
 )
 
-
-//const (
-//
-//	//Log_FILE_PATH = "logs"
-//	LOG_FILE_PATH = "/Users/a232152000/go/src/stock/logs"
-//	LOG_FILE_NAME = "system.log"
-//
-//)
-
-
 func ViperEnvVariable(key string) string {
 
 	viper.SetConfigName("env")
